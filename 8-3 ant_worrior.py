73f757645aad21621492e2dff86d1a60a9c7b251
ique-coder <tjdwns0309@gmail.com>
2020-11-11 00:34:39 +0900

    2020-11-10
    commit

--- 8-3 ant_worrior.py
+++ 8-3 ant_worrior.py
@@ -1,3 +1,11 @@
+n = int(input())
 x = list(map(int, input().split()))
 d = [0] * 100
 
+d[0] = x[0]
+d[1] = max(x[0], x[1])
+
+for i in range(2, n):
+    d[i] = max(d[i - 1], d[i - 2] + x[i])
+
+print(d[n - 1])
