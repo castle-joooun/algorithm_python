c913954ed5558210ba32f7ed26919f9b1f682e4c
ique-coder <tjdwns0309@gmail.com>
2020-11-16 20:00:33 +0900

    2020-11-16
    commit

--- *10 disjoint_sets_cycle_check.py
+++ *10 disjoint_sets_cycle_check.py
@@ -1,13 +1,13 @@
 def find_parent(parent, x):
     if parent[x] != x:
-        return find_parent(parent, parent[x])
-    # return x
+        find_parent(parent, parent[x])
     return parent[x]
 
 
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
+
     if a < b:
         parent[b] = a
     else:
@@ -18,20 +18,20 @@ def union_parent(parent, a, b):
 v, e = map(int, input().split())
 parent = [0] * (v + 1)
 
-# parent initialize is self value in parent table
 for i in range(1, v + 1):
     parent[i] = i
 
-# start union
+cycle = False
+
 for i in range(e):
     a, b = map(int, input().split())
-    union_parent(parent, a, b)
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    else:
+        union_parent(parent, a, b)
 
-for i in range(1, v + 1):
-    print(find_parent(parent, i), end=' ')
-
-print()
-
-for i in range(1, v + 1):
-    print(parent[i], end=' ')
-
+if cycle:
+    print('do cycle')
+else:
+    print('don\'t cycle')
