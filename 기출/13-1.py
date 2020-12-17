4285c3035e0d310e971ebf02748739e98a5cc4dd
ique-coder <tjdwns0309@gmail.com>
2020-12-18 00:46:48 +0900

    2020-12-18
    13 complete
    
    commit

--- 기출/13-1.py
+++ 기출/13-1.py
@@ -1,2 +1,31 @@
-n, m = map(int, input().split())
+from itertools import combinations
 
+n, m = map(int, input().split())
+chicken, house = [], []
+
+for i in range(n):
+    d = list(map(int, input().split()))
+    for j in range(n):
+        if d[j] == 1:
+            house.append((i, j))
+        elif d[j] == 2:
+            chicken.append((i, j))
+
+candidates = list(combinations(chicken, m))
+
+
+def get_sum(candidate):
+    result = 0
+    for hx, hy in house:
+        temp = 1e9
+        for cx, cy in candidate:
+            temp = min(temp, abs(hx - cx) + abs(hy - cy))
+        result += temp
+    return result
+
+
+result = 1e9
+for candidate in candidates:
+    result = min(result, get_sum(candidate))
+
+print(result)
