47e17b8e97fde00a6ecd95f5db196bae874481f8
ique-coder <tjdwns0309@gmail.com>
2020-12-22 23:57:30 +0900

    2020-12-22
    Backjoon steps
    
    commit

---
+++ Backjoon/steps/10809.py
@@ -0,0 +1,5 @@
+s = input()
+d = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for alpha in d:
+    print(s.find(alpha), end=' ')
