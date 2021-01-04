9dad9604d58adc3210f31af6e8a3b9c6c68fbef5
ique-coder <tjdwns0309@gmail.com>
2021-01-04 18:28:03 +0900

    2021-01-04
    Inflearn algo
    
    commit

---
+++ inflearn/algorithm/5/5-6_solv.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+n, m=map(int, input().split())
+Q=[(pos, val) for pos, val in enumerate(list(map(int, input().split())))]
+Q=deque(Q)
+cnt=0
+while True:
+    cur=Q.popleft()
+    if any(cur[1]<x[1] for x in Q):
+        Q.append(cur)
+    else:
+        cnt+=1
+        if cur[0]==m:
+            print(cnt)
+            break
