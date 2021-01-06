99f4e8be7418ff8b572a68408127ca0c767a3bee
ique-coder <tjdwns0309@gmail.com>
2021-01-06 23:26:22 +0900

    2021-01-06
    Inflearn algo
    
    commit

--- inflearn/algorithm/6/6-3.py
+++ inflearn/algorithm/6/6-3.py
@@ -1,9 +1,17 @@
-def dfs(n):
-    for i in range(1, n):
-        print(i)
-        dfs
+def dfs(v):
+    if v == n + 1:
+        for i in range(1, n + 1):
+            if check[i] == 1:
+                print(i, end=' ')
+        else:
+            print()
+    else:
+        check[v] = 1
+        dfs(v + 1)
+        check[v] = 0
+        dfs(v + 1)
 
 
 n = int(input())
-dfs(n)
-
+check = [0] * (n + 1)
+dfs(1)
