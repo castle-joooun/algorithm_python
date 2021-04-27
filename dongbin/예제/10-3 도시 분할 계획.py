a130496cd0e6a58450d16cfcb10dcbb8a59c8458
ique_coder <tjdwns0309@gmail.com>
2021-04-27 16:26:58 +0900

    Add: 이코테 10-3 풀이

---
+++ dongbin/예제/10-3 도시 분할 계획.py
@@ -0,0 +1,39 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+
+edges = []
+result = 0
+
+for i in range(1, n + 1):
+    parent[i] = i
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+edges.sort()
+maximum = 0
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        maximum = cost
+
+print(result - maximum)
