f9d7a9fc9fefd08fae40fb99197bc18054024ef1
ique_coder <tjdwns0309@gmail.com>
2021-05-04 20:31:36 +0900

    Add: 이코테 12-12, 카카오 풀이

---
+++ programmers/kakao/2020 KAKAO BLIND RECRUITMENT/60061.py
@@ -0,0 +1,34 @@
+def possible(answer):
+    for x, y, stuff in answer:
+        if stuff == 0:
+            if y == 0 or [x, y - 1, 0] in answer or [x, y, 1] in answer or [x - 1, y, 1] in answer:
+                continue
+            return False
+        if stuff == 1:
+            if [x, y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or ([x - 1, y, 1] in answer and [x + 1, y, 1] in answer):
+                continue
+            return False
+    return True
+
+
+def solution(n, build_frame):
+    answer = []
+
+    for frame in build_frame:
+        x, y, stuff, operate = frame
+        if operate == 0:
+            answer.remove([x, y, stuff])
+            if not possible(answer):
+                answer.append([x, y, stuff])
+        if operate == 1:
+            answer.append([x, y, stuff])
+            if not possible(answer):
+                answer.remove([x, y, stuff])
+
+    return sorted(answer)
+
+
+print(solution(5, [[1, 0, 0, 1], [1, 1, 1, 1], [2, 1, 0, 1], [2, 2, 1, 1], [5, 0, 0, 1], [5, 1, 0, 1], [4, 2, 1, 1], [
+      3, 2, 1, 1]]) == [[1, 0, 0], [1, 1, 1], [2, 1, 0], [2, 2, 1], [3, 2, 1], [4, 2, 1], [5, 0, 0], [5, 1, 0]])
+print(solution(5, [[0, 0, 0, 1], [2, 0, 0, 1], [4, 0, 0, 1], [0, 1, 1, 1], [1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1, 1], [
+      2, 0, 0, 0], [1, 1, 1, 0], [2, 2, 0, 1]]) == [[0, 0, 0], [0, 1, 1], [1, 1, 1], [2, 1, 1], [3, 1, 1], [4, 0, 0]])
