dd87fd87837c5426d7ca959ecb9e72d7f9d02dc7
ique-coder <tjdwns0309@gmail.com>
2022-03-24 23:43:27 +0900

    backjoon: 10845 solve

--- Backjoon/beginner 1/1158. josephus.py
+++ Backjoon/beginner 1/1158. josephus.py
@@ -1,26 +1,14 @@
 N, K = map(int, input().split())
+arr = [i for i in range(1, N + 1)]
 
-visited = [False for _ in range(N)]
 result = []
-index = 1
+index = 0
 
-while False in visited:
-    count = 1
-    while True:
-        if index > N:
-            index -= N
+for _ in range(N):
+    index += K - 1
+    if index >= len(arr):
+        index %= len(arr)
 
-        if count == 3 and not visited[index - 1]:
-            result.append(str(index))
-            visited[index - 1] = True
-            index += 1
-            break
-        else:
-            if visited[index - 1]:
-                index += 1
-                continue
-
-            count += 1
-            index += 1
+    result.append(str(arr.pop(index)))
 
 print(f'<{", ".join(result)}>')
