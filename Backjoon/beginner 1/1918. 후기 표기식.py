6dfb9619b78303b507ec1c5a1057beb76455671f
ique-coder <tjdwns0309@gmail.com>
2022-03-28 23:48:52 +0900

    backjoon: 1918 solve

---
+++ Backjoon/beginner 1/1918. 후기 표기식.py
@@ -0,0 +1,27 @@
+cal = input()
+
+stack = []
+result = ''
+
+index = 0
+for s in cal:
+    if s.isalpha():
+        result += s
+    else:
+        if s == '(':
+            stack.append(s)
+        elif s == '*' or s == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                result += stack.pop()
+            stack.append(s)
+        elif s == '+' or s == '-':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.append(s)
+        elif s == ')':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.pop()
+while stack:
+    result += stack.pop()
+print(result)
