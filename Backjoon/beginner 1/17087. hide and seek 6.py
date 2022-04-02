0acb093fb96d481b1d9b788d59bc5bebe8042ac1
ique-coder <tjdwns0309@gmail.com>
2022-04-02 20:06:12 +0900

    backjoon: 17087 solve

--- Backjoon/beginner 1/17087. hide and seek 6.py
+++ Backjoon/beginner 1/17087. hide and seek 6.py
@@ -1,22 +1,15 @@
-from itertools import combinations
+def find_gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
 
 N, S = map(int,input().split())
 l = list(map(int, input().split()))
+check = list(set(abs(l[i] - S) for i in range(N)))
 
-if len(l) == 1:
-    print(abs(l[0] - S))
-else:
-    check = [abs(a - b) for a, b in list(combinations(l, 2))]
+gcd_arr = check[0]
+for i in range(len(check)):
+    gcd_arr = find_gcd(gcd_arr, check[i])
 
-
-    def find_gcd(a, b):
-        while b != 0:
-            a, b = b, a % b
-        return a
-
-
-    gcd_arr = check[0]
-    for i in range(len(check)):
-        gcd_arr = find_gcd(gcd_arr, check[i])
-
-    print(gcd_arr)
+print(gcd_arr)
