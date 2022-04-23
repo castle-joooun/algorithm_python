3ddeb3efff4b0ab221d0b2c143b9b66b328b89be
ique-coder <tjdwns0309@gmail.com>
2022-04-24 00:35:24 +0900

    backjoon: 1149 solve

--- Backjoon/beginner 1/1149. RGB거리.py
+++ Backjoon/beginner 1/1149. RGB거리.py
@@ -2,28 +2,11 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-l = [[0, 0, 0]] + [list(map(int, input().split())) for _ in range(N)]
-dp = [(-1, 10**8)] * (N + 1)
-dp[0] = (-1, 0)
-answer = []
+dp = [list(map(int, input().split())) for _ in range(N)]
 
+for i in range(1, N):
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + dp[i][0]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + dp[i][1]
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + dp[i][2]
 
-for t in range(3):
-    dp = [(-1, 10 ** 8)] * (N + 1)
-    dp[0] = (-1, 0)
-    dp[1] = (t, l[1][t])
-
-    for i in range(2, N + 1):
-        for j in range(3):
-            if dp[i - 1][0] == j:
-                continue
-
-            check = dp[i][1]
-            check = dp[i - 1][1]
-            check = l[i][j]
-            if dp[i][1] > dp[i - 1][1] + l[i][j]:
-                dp[i] = (j, dp[i - 1][1] + l[i][j])
-
-    answer.append(dp[N][1])
-
-print(min(answer))
+print(min(dp[N - 1]))
