24500f1bcb6d989423d00282aa52d8c525fcfe45
ique-coder <tjdwns0309@gmail.com>
2022-04-23 23:53:51 +0900

    backjoon: 15988 solve

--- Backjoon/beginner 1/15988. 1, 2, 3 더하기 3.py
+++ Backjoon/beginner 1/15988. 1, 2, 3 더하기 3.py
@@ -1,19 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-dp = [0] * 1_000_001
+T = int(input())
+l = [int(input()) for _ in range(T)]
+
+dp = [0] * (max(l) + 1)
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
 
-for _ in range(int(input())):
-    N = int(input())
+for i in range(4, max(l) + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1_000_000_009
 
-    for i in range(4, N + 1):
-        if dp[i] != 0:
-            continue
-
-        dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1_000_000_009
-
-    print(dp[N])
-
+print(*[dp[N] for N in l], sep='\n')
