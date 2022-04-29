7073e71e8c0ae9074fb7820210292c25f6e653bf
ique-coder <tjdwns0309@gmail.com>
2022-04-29 14:25:22 +0900

    backjoon: 9465 solve

--- Backjoon/beginner 1/9465. 스티커.py
+++ Backjoon/beginner 1/9465. 스티커.py
@@ -4,6 +4,10 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     N = int(input())
     dp = [list(map(int, input().split())) for _ in range(2)]
+    if N == 1:
+        print(max(dp[0][0], dp[1][0]))
+        continue
+
     dp[0][1] += dp[1][0]
     dp[1][1] += dp[0][0]
 
