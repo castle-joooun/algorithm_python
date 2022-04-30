1d553cb76afb947b47a2674dc6096d5194b4fced
ique-coder <tjdwns0309@gmail.com>
2022-05-01 00:19:08 +0900

    backjoon: 2156 solve

--- Backjoon/beginner 1/2156. 포도주 시식.py
+++ Backjoon/beginner 1/2156. 포도주 시식.py
@@ -1,22 +1,11 @@
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-l = [0] + [int(input()) for _ in range(N)]
-dp = [0] * (N + 1)
-if N == 1:
-    print(l[1])
-elif N == 2:
-    print(l[1] + l[2])
-else:
-    dp[1] = (1, l[1])
-    dp[2] = (2, l[1] + l[2])
-    for i in range(3, N + 1):
-        check1 = check2 = (0, 0)
-        if dp[i - 1][0] < 2:
-            check1 = (dp[i - 1][0] + 1, dp[i - 1][1] + l[i])
-        check2 = (0, dp[i - 1][1])
-
-        dp[i] = check1 if check1[1] > check2[1] else check2
-
-    print(dp[N][1])
+n = int(input())
+w = [0]
+for i in range(n):
+    w.append(int(input()))
+dp = [0]
+dp.append(w[1])
+if n > 1:
+    dp.append(w[1] + w[2])
+for i in range(3, n + 1):
+    dp.append(max(dp[i - 1], dp[i - 3] + w[i - 1] + w[i], dp[i - 2] + w[i]))
+print(dp[n])
