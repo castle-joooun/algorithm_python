7e0d23847b76eed03b6117f67323a955d2e7de50
castle-joooun <tjdwns0309@gmail.com>
2023-07-17 01:42:41 +0900

    all_in_one
    linked_array 수정

--- inflearn/all_in_one/02_Linked_List(init).py
+++ inflearn/all_in_one/02_Linked_List(init).py
@@ -1,45 +1,59 @@
 class Node:
-    def __init__(self, value = 0, next = None):
+    def __init__(self, value=0, next=None, prev=None):
         self.value = value
         self.next = next
+        self.prev = prev
 
 
 class LinkedList(object):
     def __init__(self):
         self.head = None
+        self.tail = None
 
     def append(self, value):
         new_node = Node(value)
-        if self.head is not None:
+        if not self.head:
             self.head = new_node
+            self.tail = new_node
         else:
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
             current = self.head
             while current.next:
+                current.next.prev = current
                 current = current.next
-            current.next = new_node
 
     def get(self, idx):
-        current = self.haed
+        current = self.head
         for _ in range(idx):
-            if current.next is not None:
+            if current.next:
                 current = current.next
-        return current
+        return current.value
 
     def insert(self, idx, value):
-        current = self.haed
-        prev = None
+        current = self.head
         for _ in range(idx):
-            prev = current
             current = current.next
-        new_node = Node(value, current.next)
-        prev.next = new_node
+        new_node = Node(value, current, current.prev)
+        current.prev.next = new_node
 
     def remove(self, idx):
         current = self.head
-        prev = None
         for _ in range(idx):
-            prev = current
             current = current.next
-        prev.next = current
+
+        print(current.prev)
+        current.prev.next = current.next
 
 
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+print(ll.get(1))
+ll.insert(1, 5)
+print(ll.get(1))
+ll.remove(1)
+print(ll.get(1))
