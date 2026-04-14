33fc1a68be0898cad94c45217d3528a71133dba5
castle-joooun <51706303+castle-joooun@users.noreply.github.com>
2026-04-14 17:41:28 +0900

    Title: 달에서 무게 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 13;
+        double b = 0.165;
+        System.out.print(a + " * ");
+        System.out.printf("%.6f", b);
+        System.out.printf(" = %.6f", a * b);
+    }
+}
