3e27f5301875717ec3ef9ea9384d3afd4e823e0f
castle-joooun <51706303+castle-joooun@users.noreply.github.com>
2026-04-14 17:48:22 +0900

    Title: 실수 받아 그대로 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        System.out.printf("%.2f", n);
+    }
+}
