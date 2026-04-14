ebe6fd7b3e04e136ea8f1875677026be789f026f
castle-joooun <51706303+castle-joooun@users.noreply.github.com>
2026-04-14 17:45:11 +0900

    Title: 점수 입출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Your score is " + n + " point.");
+    }
+}
