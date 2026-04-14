da1b75b6cd71c5389f8ec86d3fcbfd326172a7aa
castle-joooun <51706303+castle-joooun@users.noreply.github.com>
2026-04-14 18:28:11 +0900

    Title: 문자 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/문자 출력하기/print-text.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        for (int i = 0; i < 8; i++) {
+            System.out.print(s);
+        }
+    }
+}
