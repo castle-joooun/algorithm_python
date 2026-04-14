befd5c2da3a9d0d78228afb9d2faf259b899395a
castle-joooun <51706303+castle-joooun@users.noreply.github.com>
2026-04-14 17:28:36 +0900

    Title: 문자 변경하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260414/문자 변경하기/change-charater.java
+++ 260414/문자 변경하기/change-charater.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        String a = 'C';
-        String a = 'T';
+        String a = "C";
+        a = "T";
         System.out.println(a);
     }
 }
