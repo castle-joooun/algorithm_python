c8daf00187e74a31e84366925b3b70d3443aebd4
castle-joooun <51706303+castle-joooun@users.noreply.github.com>
2026-04-14 17:27:49 +0900

    Title: 문자 변경하기, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260414/문자 변경하기/change-charater.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Char a = 'C';
+        Char a = 'T';
+        System.out.println(a);
+    }
+}
