d05828d54b8c9716fa979938518696a9bc8dd73f
castle-joooun <51706303+castle-joooun@users.noreply.github.com>
2026-04-14 17:35:40 +0900

    Title: 변수 출력하기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 1;
+        int b = 2;
+        char c = 'C';
+        System.out.println(a + "->" + b + "->" + c);
+    }
+}
