c085b0029d15044f24a634e3185197b94bde273f
castle-joooun <51706303+castle-joooun@users.noreply.github.com>
2026-04-14 17:54:19 +0900

    Title: 입력받은 값 교체하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.println(b + " " + a);
+    }
+}
