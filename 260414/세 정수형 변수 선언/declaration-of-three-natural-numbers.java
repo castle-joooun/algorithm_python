4b9ac0dbee9460839eedec750c2c45f55114bfaf
castle-joooun <51706303+castle-joooun@users.noreply.github.com>
2026-04-14 17:37:06 +0900

    Title: 세 정수형 변수 선언, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 7;
+        int b = 23;
+        int c = 30;
+        System.out.println(a + " + " + b + " = " + c);
+    }
+}
