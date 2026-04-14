e1c4c11f66deff0a3b6898c07cd710a399d2548d
castle-joooun <51706303+castle-joooun@users.noreply.github.com>
2026-04-14 17:33:12 +0900

    Title: 사칙연산 연습, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 30;
+        int c = a + b;
+        System.out.println(c / 8);
+        System.out.println(c % 8);
+    }
+}
